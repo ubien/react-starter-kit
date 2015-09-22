34231240015ed823260dfe46a0701b252c58af02
Jacob Mortensen <jacob@yourmemoir.com>
2015-09-22 14:01:05 -0400

    remove extra pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,14 +26,6 @@ class Footer extends Component {
       <div className="Footer">
         <div className="Footer-container">
           <span className="Footer-text">© Your Company</span>
-          <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/" onClick={Link.handleClick}>Home</a>
-          <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/privacy" onClick={Link.handleClick}>Privacy</a>
-          <span className="Footer-spacer">·</span>
-          <a className="Footer-link" href="/not-found" onClick={Link.handleClick}>Not Found</a>
-          <span className="Footer-spacer"> | </span>
-          <span ref="viewport" className="Footer-viewport Footer-text Footer-text--muted">Viewport:</span>
         </div>
       </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import styles from './Header.css';
 import withStyles from '../../decorators/withStyles';
 import Link from '../Link';
-import Navigation from '../Navigation';
 
 @withStyles(styles)
 class Header extends Component {
@@ -17,7 +16,6 @@ class Header extends Component {
             <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
             <span className="Header-brandTxt">Your Company</span>
           </a>
-          <Navigation className="Header-nav" />
           <div className="Header-banner">
             <h1 className="Header-bannerTitle">React</h1>
             <p className="Header-bannerDesc">Complex web apps made easy</p>
